2380084375cbffc05b4d9faac4c051e8098d7e52
hoshi4444 <hoshi44444444@gmail.com>
2019-10-15 20:16:09 +0900

    edit-slideber

---
+++ app/assets/javascripts/slickers.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load',function(){
+
+  $('.sliders').slick({
+    autoplay:true,
+    dots:true,
+    // prevArrow: 
+    // // nextArrow:'<img src="次への矢印画像のパス" class="slide-arrow next-arrow">'
+    
+
+  });
+  
+  });
