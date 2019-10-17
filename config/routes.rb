15ed7df700d2da930e2f55984f2f3e811761f1ba
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-10-17 17:08:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   get "items/item"
+  get "signups/registration"
   resources :signups do
     collection do
-      get "registration"
       get "newmember"
       get "phonenumber"
-      get "authentication"
       get "address"
       get "payment"
       get "completion"
