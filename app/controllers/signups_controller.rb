655940ce671b775d9603ce68cfe06a0680ac5406
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-10-17 13:59:16 +0900

    mark_up view

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -19,4 +19,42 @@ class SignupsController < ApplicationController
 
   def completion
   end
+
+  private
+    def user_params
+    params.require(:user).permit(
+      :nickname,
+      :email,
+      :password,
+      :password_confirmation,
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :birthdate_year,
+      :birthdate_month,
+      :birthdate_day,
+      :phone_number,
+      :address_last_name,
+      :address_first_name,
+      :address_last_name_kana,
+      :address_first_name_kana,
+      :postal_coad,
+      :prefecture,
+      :city,
+      :block_number,
+      :building_name,
+      :address_phone_number,
+      :card_number,
+      :card_security_code,
+      :card_expiration_date_month,
+      :card_expiration_date_year,
+      :introduce,
+      :reset_password_token,
+      :reset_password_sent_at,
+      :remember_created_at,
+      :created_at,
+      :updated_at
+    )
+  end
 end
