286bc027e664d0a0315d3c30b136c6af568fd2e5
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-10-28 17:44:11 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :omniauth_callbacks =>  "users/omniauth_callbacks"
+  }
   root "items#index"
   get "items/item"
   resources :signups, only: [:create] do
