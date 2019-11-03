f3691beb3084621dc6869c86926d22aab46b9d32
goodluckpenpen <neipia_number@icloud.com>
2019-11-03 21:42:52 +0900

    image

--- db/seeds.rb
+++ db/seeds.rb
@@ -359,7 +359,7 @@
 # end
 
 20.times do |no|
-  Item.create!(title:"randomItem #{no}",user_id:"1",size_id:"#{rand(1..22)}",condition_id:"#{rand(1..6)}",delivery_fee_id:"#{rand(1..2)}",delivery_method_id:"#{rand(1..11)}",delivery_area_id:"#{rand(1..48)}",delivery_day_id:"#{rand(1..3)}",brand_id:"#{rand(1..5)}",category_id:"#{rand(1..14)}",price:"#{rand(1..1000000)}",text:"あ",buyer_id:"2",seller_id:"1")
+  Item.create!(title:"randomItem #{no + 1}",user_id:"1",size_id:"#{rand(1..22)}",condition_id:"#{rand(1..6)}",delivery_fee_id:"#{rand(1..2)}",delivery_method_id:"#{rand(1..11)}",delivery_area_id:"#{rand(1..48)}",delivery_day_id:"#{rand(1..3)}",brand_id:"#{rand(1..5)}",category_id:"#{rand(1..14)}",price:"#{rand(1..1000000)}",text:"あ",buyer_id:"2",seller_id:"1")
 end
 
 # 10.times do |no|
@@ -370,10 +370,10 @@ end
 #   Item.create(title:"randomItem #{no}",user_id:"1",size_id:"#{rand(1..22)}",condition_id:"#{rand(1..6)}",delivery_fee_id:"#{rand(1..2)}",delivery_method_id:"#{rand(1..11)}",delivery_area_id:"#{rand(1..48)}",delivery_day_id:"#{rand(1..3)}",brand_id:"#{rand(1..5)}",category_id:"#{rand(1..2062)}",price:"#{rand(1..1000000)}")
 # end
 
-# array = ["https://gyazo.com/d70b9be2225c7c7d1a70edd2d62dc713","https://gyazo.com/d70b9be2225c7c7d1a70edd2d62dc713","https://gyazo.com/d70b9be2225c7c7d1a70edd2d62dc713"]
-# 30.times do |no|
-#   Image.create!(image:"#{array[rand(3)]}",item_id:"#{no + 1}")
-# end
+array = ["https://gyazo.com/d70b9be2225c7c7d1a70edd2d62dc713","https://gyazo.com/d70b9be2225c7c7d1a70edd2d62dc713","https://gyazo.com/d70b9be2225c7c7d1a70edd2d62dc713"]
+30.times do |no|
+  Image.create!(image:"#{array[rand(3)]}",item_id:"#{no + 1}")
+end
 
 # array = ["B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg"]
 # 5000.times do |no|
