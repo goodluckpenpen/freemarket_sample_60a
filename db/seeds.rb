aca3f47779ca2c772c9e421752fe2b79c6dd3755
goodluckpenpen <neipia_number@icloud.com>
2019-11-04 15:04:59 +0900

    item

--- db/seeds.rb
+++ db/seeds.rb
@@ -359,7 +359,7 @@
 # end
 
 500.times do |no|
-  Item.create(title:"randomItem #{no}",user_id:"1",size_id:"#{rand(1..22)}",condition_id:"#{rand(1..6)}",delivery_fee_id:"#{rand(1..2)}",delivery_method_id:"#{rand(1..11)}",delivery_area_id:"#{rand(1..48)}",delivery_day_id:"#{rand(1..3)}",brand_id:"#{rand(1..5)}",category_id:"#{rand(1..14)}",price:"#{rand(1..1000000)}",text:"あ",buyer_id:"2",seller_id:"1")
+  Item.create!(title:"randomItem #{no}",user_id:"1",size_id:"#{rand(1..22)}",condition_id:"#{rand(1..6)}",delivery_fee_id:"#{rand(1..2)}",delivery_method_id:"#{rand(1..11)}",delivery_area_id:"#{rand(1..48)}",delivery_day_id:"#{rand(1..3)}",brand_id:"#{rand(1..5)}",category_id:"#{rand(1..14)}",price:"#{rand(1..1000000)}",text:"あ",buyer_id:"2",seller_id:"1")
 end
 
 # 10.times do |no|
@@ -379,7 +379,7 @@ end
 #   "https://images-na.ssl-images-amazon.com/images/I/41qJb2tJMIL._AC_.jpg",
 #   "https://images-na.ssl-images-amazon.com/images/I/31kwsNfHchL.jpg"]
 # 500.times do |no|
-#   Image.create!(image:"#{array[rand(3)]}",item_id:"#{no + 1261}")
+#   Image.create!(image:"#{array[rand(3)]}",item_id:"#{no + 1221}")
 # end
 
 # array = ["B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg","B9nQcbKkRWKoJ9Q4gUm%bQ.jpg"]
