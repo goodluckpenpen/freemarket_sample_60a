1c663db8cd4e41aad87e8441604f6280a261a8a0
goodluckpenpen <neipia_number@icloud.com>
2019-11-21 20:10:19 +0900

    image

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -78,7 +78,7 @@ class ItemsController < ApplicationController
     # binding.pry
     # インスタンスの保存に成功した場合の処理
      if @item.save!
-      redirect_to root_path
+      redirect_back(fallback_location: new_item_path)
     # インスタンスの保存に失敗した場合の処理
     else
       render :new
